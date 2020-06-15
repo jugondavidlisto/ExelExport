256831f8fcb7dace4faf10b984805021775ec184
jugondavidlisto <dfusteg@gmail.com>
2020-06-15 12:49:29 +0200

    Primer cambio

---
+++ ExelExport/Form1.Designer.vb
@@ -0,0 +1,79 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.dgvDatos = New System.Windows.Forms.DataGridView()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        CType(Me.dgvDatos, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(13, 415)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Test"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(95, 415)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Abrir"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'dgvDatos
+        '
+        Me.dgvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvDatos.Location = New System.Drawing.Point(13, 13)
+        Me.dgvDatos.Name = "dgvDatos"
+        Me.dgvDatos.Size = New System.Drawing.Size(775, 396)
+        Me.dgvDatos.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.dgvDatos)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.dgvDatos, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents dgvDatos As DataGridView
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+End Class
---
+++ ExelExport/Form1.vb
@@ -0,0 +1,244 @@
+﻿Imports System.IO
+Imports System.Data
+Imports System.Data.OleDb
+Imports Microsoft.Office.Interop
+Imports System.Security.Authentication.ExtendedProtection
+
+Public Class Form1
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim dtOrigenOrigen As DataTable = Nothing
+
+        Try
+            If FolderBrowserDialog1.ShowDialog = DialogResult.OK Then
+                Dim txtRuta = FolderBrowserDialog1.SelectedPath
+                Debug.WriteLine(txtRuta)
+
+                Dim ApExel = New Excel.Application
+                'Dim nBook = ApExel.Workbooks.Add
+                'Dim nSheet = ApExel.Worksheets(1)
+                Dim nBook As Excel.Workbook
+                Dim nSheet As Excel.Worksheet
+
+                nBook = ApExel.Workbooks.Add
+
+                If ApExel.Application.Sheets.Count < 1 Then
+                    nSheet = CType(nBook.Worksheets.Add, Excel.Worksheet)
+                Else
+                    nSheet = ApExel.Worksheets(1)
+                    nSheet.Name = "Hoja1"
+                End If
+
+                Dim oExel = New Excel.Application
+                Dim ApExel_TitleSet As Boolean = False
+
+                Dim columna As Integer = 2
+
+                For Each files As String In Directory.GetFiles(txtRuta)
+
+                    Dim strcamino As String = files
+                    oExel.Workbooks.Open(strcamino)
+                    Dim oBook As Object
+                    Dim oSheet As Object
+                    oBook = oExel.ActiveWorkbook
+                    oSheet = oExel.Worksheets(2)
+
+                    'Dim concepto = oSheet.Range("A4:A24").Copy()
+
+                    Debug.WriteLine(files)
+                    Debug.WriteLine(Path.GetFileNameWithoutExtension(files))
+                    Debug.WriteLine(Path.GetFileNameWithoutExtension(files).Substring(6, 8))
+                    Dim fecha = Path.GetFileNameWithoutExtension(files).Substring(6, 8)
+
+                    Debug.WriteLine(fecha.Substring(0, 4))
+                    Debug.WriteLine(fecha.Substring(4, 2))
+                    Debug.WriteLine(fecha.Substring(6, 2))
+                    Dim ano = fecha.Substring(0, 4)
+                    Dim mes = fecha.Substring(4, 2)
+                    Dim dia = fecha.Substring(6, 2)
+
+
+                    If nSheet.Name = "Hoja1" Then
+                        nSheet.Name = "G" & mes
+                    End If
+
+                    Debug.WriteLine("Sheet Name: " & nSheet.Name.Substring(1, 2))
+                    Debug.WriteLine("Sheet Count: " & ApExel.Application.Sheets.Count)
+                    If mes > nSheet.Name.Substring(1, 2) And dia = 1 Then
+                        nSheet = CType(nBook.Worksheets.Add, Excel.Worksheet)
+                        'nSheet = ApExel.Worksheets(nSheet.Count)
+                        columna = 2
+                        nSheet = nBook.Worksheets(ApExel.Application.Sheets.Count)
+                        nSheet.Name = "G" & mes
+                        ApExel_TitleSet = False
+                    End If
+
+                    'Copia de la columna de titulos
+                    If ApExel_TitleSet = False Then
+                        oSheet.Range("A4:A25").Copy()
+
+                        'Dim titulo = oSheet.Sheets(1).Range("A1").Copy()
+                        nSheet.Range("A1").PasteSpecial(Excel.XlPasteType.xlPasteValues)
+                        With nSheet.Columns("A")
+                            .ColumnWidth = 36
+                        End With
+                        'nSheet.Columns("A").EntireColumn.AutoFit()
+                        nSheet.Range("A23").Value = "Fecha"
+                        nSheet.Range("A23").Font.Name = "Arial"
+                        nSheet.Range("A23").Font.Size = 8
+
+                        ApExel_TitleSet = True
+                    End If
+
+                    'Copia de columna de information
+                    oSheet.Range("C4:C25").Copy()
+                    nSheet.Cells(1, columna).PasteSpecial(Excel.XlPasteType.xlPasteValues)
+                    With nSheet.Columns(columna)
+                        .ColumnWidth = 15
+                    End With
+
+                    'Fotmateo de la fecha
+                    Dim fechaFinal As String = dia & "-" & mes & "-" & ano
+                    nSheet.Cells(23, columna).Value = DateSerial(Month:=mes, Day:=dia, Year:=ano)
+                    nSheet.Cells(23, columna).NumberFormat = "dd-mmm"
+                    nSheet.Cells(23, columna).font.Name = "Arial"
+                    nSheet.Cells(23, columna).font.Size = 8
+
+                    columna += 1
+
+                    nSheet.Move(After:=nBook.Worksheets(nBook.Worksheets.Count))
+
+                    oBook.Close()
+                    oBook = Nothing
+
+                Next
+
+                'Libro.Sheets(1).Cells(2, 1) = "hola test"
+
+                SaveFileDialog1.DefaultExt = "*.xlsx"
+                SaveFileDialog1.FileName = "PrecioElectricidad"
+                SaveFileDialog1.Filter = "Archivos de Exel (*.xlsx)|*.xlsx"
+                SaveFileDialog1.ShowDialog()
+
+                nBook.SaveAs(SaveFileDialog1.FileName)
+                MessageBox.Show("El archivo se creo y se guardo en:" & vbCrLf & SaveFileDialog1.FileName)
+
+                nBook.Close()
+                'ApExel.Quit()
+                nBook = Nothing
+                ApExel = Nothing
+
+                'Dim ApExel = New Microsoft.Office.Interop.Excel.Application
+                'Dim Libro = ApExel.Workbooks.Add
+
+                'Libro.Sheets(1).Cells(2, 1) = "hola test"
+
+                'SaveFileDialog1.DefaultExt = "*.xlsx"
+                'SaveFileDialog1.FileName = "PrecioElectricidad"
+                'SaveFileDialog1.Filter = "Archivos de Exel (*.xlsx)|*.xlsx"
+                'SaveFileDialog1.ShowDialog()
+
+                'Libro.SaveAs(SaveFileDialog1.FileName)
+                'MessageBox.Show("El archivo se creo y se guardo en:" & vbCrLf & SaveFileDialog1.FileName)
+
+                'ApExel.Quit()
+                'Libro = Nothing
+                'ApExel = Nothing
+
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+        'Try
+        '    Dim ofdiag As New System.Windows.Forms.OpenFileDialog
+        '    ofdiag.Filter = "archivos exel |*.xls;*.xlsx"
+        '    If ofdiag.ShowDialog = DialogResult.OK Then
+        '        Dim strcamino As String = ofdiag.FileName
+        '        Dim strconn As String = "provider=microsoft.ace.oledb.12.0;" & "data source =" & strcamino & "; extended properties=""excel 12.0;hdr=no;imex=1"""
+
+        '        Dim conn As OleDb.OleDbConnection = New OleDb.OleDbConnection
+        '        conn.ConnectionString = strconn
+        '        conn.Open()
+
+        '        Dim da As OleDb.OleDbDataAdapter = New OleDb.OleDbDataAdapter("select * from [" & "i3dia01" & "$c5:c25]", conn)
+        '        dtOrigenOrigen = New DataTable
+
+        '        da.Fill(dtOrigenOrigen)
+
+        '        dgvDatos.DataSource = dtOrigenOrigen
+        '        dgvDatos.Refresh()
+        '    End If
+        'Catch ex As Exception
+        '    MessageBox.Show(ex.Message)
+        'End Try
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim fileTest As String = "C:\Users\Davidf\Desktop\test.xlsx"
+        If File.Exists(fileTest) Then
+            File.Delete(fileTest)
+        End If
+
+        Dim oExcel As Object
+        oExcel = CreateObject("Excel.Application")
+        Dim oBook As Excel.Workbook
+        Dim oSheet As Excel.Worksheet
+
+        oBook = oExcel.Workbooks.Add
+
+        ' first worksheet
+        If oExcel.Application.Sheets.Count() < 1 Then
+            oSheet = CType(oBook.Worksheets.Add(), Excel.Worksheet)
+        Else
+            oSheet = oExcel.Worksheets(1)
+        End If
+        oSheet.Name = "one"
+        oSheet.Range("B1").Value = "First One"
+
+        oSheet.Move(After:=oBook.Worksheets(oBook.Worksheets.Count))
+
+        ' second
+        If oExcel.Application.Sheets.Count() < 2 Then
+            oSheet = CType(oBook.Worksheets.Add(), Excel.Worksheet)
+        Else
+            oSheet = oExcel.Worksheets(2)
+        End If
+        oSheet.Name = "two"
+        oSheet.Range("B1").Value = "Second one"
+
+        oSheet.Move(After:=oBook.Worksheets(oBook.Worksheets.Count))
+
+        ' third
+        If oExcel.Application.Sheets.Count() < 3 Then
+            oSheet = CType(oBook.Worksheets.Add(), Excel.Worksheet)
+        Else
+            oSheet = oExcel.Worksheets(3)
+        End If
+        oSheet.Name = "three"
+        oSheet.Range("B1").Value = "Thrid"
+
+        oSheet.Move(After:=oBook.Worksheets(oBook.Worksheets.Count))
+
+        ' next
+        If oExcel.Application.Sheets.Count() < 4 Then
+            oSheet = CType(oBook.Worksheets.Add(), Excel.Worksheet)
+        Else
+            oSheet = oExcel.Worksheets(4)
+        End If
+        oSheet.Name = "four"
+        oSheet.Range("B1").Value = "Four"
+
+        ' by default this worksheet will be placed in front of the first
+        ' the code below will move it after third one
+
+        oSheet.Move(After:=oBook.Worksheets(oBook.Worksheets.Count))
+
+        oBook.SaveAs(fileTest)
+        oBook.Close()
+        oBook = Nothing
+
+        MessageBox.Show("Fin")
+
+    End Sub
+End Class

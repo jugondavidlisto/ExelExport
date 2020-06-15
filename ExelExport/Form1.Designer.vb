009ebb040589d07eb31eb3fffad983e8025afdf9
jugondavidlisto <dfusteg@gmail.com>
2020-06-15 17:29:10 +0200

    File export fix

--- ExelExport/Form1.Designer.vb
+++ ExelExport/Form1.Designer.vb
@@ -22,12 +22,14 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
-        Me.dgvDatos = New System.Windows.Forms.DataGridView()
         Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
         Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
-        CType(Me.dgvDatos, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Button1
@@ -48,32 +50,55 @@ Partial Class Form1
         Me.Button2.Text = "Abrir"
         Me.Button2.UseVisualStyleBackColor = True
         '
-        'dgvDatos
+        'ProgressBar1
         '
-        Me.dgvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dgvDatos.Location = New System.Drawing.Point(13, 13)
-        Me.dgvDatos.Name = "dgvDatos"
-        Me.dgvDatos.Size = New System.Drawing.Size(775, 396)
-        Me.dgvDatos.TabIndex = 2
+        Me.ProgressBar1.Location = New System.Drawing.Point(13, 31)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(775, 23)
+        Me.ProgressBar1.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(385, 57)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(54, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Finalizado"
+        Me.Label1.Visible = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(13, 57)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(24, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "0 %"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.dgvDatos)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ProgressBar1)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Form1"
         Me.Text = "Form1"
-        CType(Me.dgvDatos, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
-    Friend WithEvents dgvDatos As DataGridView
     Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
     Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
 End Class
--- ExelExport/Form1.vb
+++ ExelExport/Form1.vb
@@ -6,40 +6,49 @@ Imports System.Security.Authentication.ExtendedProtection
 
 Public Class Form1
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        Dim dtOrigenOrigen As DataTable = Nothing
+        'Variables Exel Export
+        Dim exportExel = New Excel.Application
+        Dim exportBook As Excel.Workbook
+        Dim exportSheet As Excel.Worksheet
+        Dim ApExel_TitleSet As Boolean = False
+        Dim columna As Integer = 2
 
-        Try
-            If FolderBrowserDialog1.ShowDialog = DialogResult.OK Then
-                Dim txtRuta = FolderBrowserDialog1.SelectedPath
-                Debug.WriteLine(txtRuta)
+        'Creacion de la Hoja de Exel Export y seteo del nombre por defecto
+        exportBook = exportExel.Workbooks.Add
 
-                Dim ApExel = New Excel.Application
-                Dim nBook As Excel.Workbook
-                Dim nSheet As Excel.Worksheet
+        If exportExel.Application.Sheets.Count < 1 Then
+            exportSheet = CType(exportBook.Worksheets.Add, Excel.Worksheet)
+        Else
+            exportSheet = exportExel.Worksheets(1)
+            exportSheet.Name = "Hoja1"
+        End If
 
-                nBook = ApExel.Workbooks.Add
+        'Variables Exel Original
+        Dim originalExel = New Excel.Application
+        Dim originalBook As Object
+        Dim originalSheet As Object
 
-                If ApExel.Application.Sheets.Count < 1 Then
-                    nSheet = CType(nBook.Worksheets.Add, Excel.Worksheet)
-                Else
-                    nSheet = ApExel.Worksheets(1)
-                    nSheet.Name = "Hoja1"
-                End If
+        'Funcion principal
+        If FolderBrowserDialog1.ShowDialog = DialogResult.OK Then
+            Dim txtRuta = FolderBrowserDialog1.SelectedPath
+            Debug.WriteLine(txtRuta)
 
-                Dim oExel = New Excel.Application
-                Dim ApExel_TitleSet As Boolean = False
+            'Variables Barra de Progreso
+            Dim Cuenta As Integer = 0
+            ProgressBar1.Maximum = Directory.GetFiles(txtRuta).Count
+            Label1.Visible = False
+            Label2.Text = 0 & (" %")
 
-                Dim columna As Integer = 2
-
+            Try
                 For Each files As String In Directory.GetFiles(txtRuta)
 
                     Dim strcamino As String = files
-                    oExel.Workbooks.Open(strcamino)
-                    Dim oBook As Object
-                    Dim oSheet As Object
-                    oBook = oExel.ActiveWorkbook
-                    oSheet = oExel.Worksheets(2)
+                    originalExel.Workbooks.Open(strcamino)
 
+                    originalBook = originalExel.ActiveWorkbook
+                    originalSheet = originalExel.Worksheets(2)
+
+                    ' Obtencion de la fecha actual apartir de los nombres de archivo
                     Debug.WriteLine(files)
                     Debug.WriteLine(Path.GetFileNameWithoutExtension(files))
                     Debug.WriteLine(Path.GetFileNameWithoutExtension(files).Substring(6, 8))
@@ -52,78 +61,88 @@ Public Class Form1
                     Dim mes = fecha.Substring(4, 2)
                     Dim dia = fecha.Substring(6, 2)
 
-
-                    If nSheet.Name = "Hoja1" Then
-                        nSheet.Name = "G" & mes
+                    ' Correcion del nombre de la Hoja del Exel Export por el formato correcto G+numeroMes
+                    If exportSheet.Name = "Hoja1" Then
+                        exportSheet.Name = "G" & mes
                     End If
 
-                    Debug.WriteLine("Sheet Name: " & nSheet.Name.Substring(1, 2))
-                    Debug.WriteLine("Sheet Count: " & ApExel.Application.Sheets.Count)
-                    If mes > nSheet.Name.Substring(1, 2) And dia = 1 Then
-                        nSheet = CType(nBook.Worksheets.Add, Excel.Worksheet)
+                    ' Condicional para la creacion de una nueva hoja si a terminado de llenar un mes
+                    If mes > exportSheet.Name.Substring(1, 2) And dia = 1 Then
+                        exportSheet = CType(exportBook.Worksheets.Add(), Excel.Worksheet)
                         'nSheet = ApExel.Worksheets(nSheet.Count)
                         columna = 2
-                        nSheet = nBook.Worksheets(ApExel.Application.Sheets.Count)
-                        nSheet.Name = "G" & mes
+                        'exportSheet = exportBook.Worksheets(exportExel.Application.Sheets.Count)
+                        exportSheet.Name = "G" & mes
                         ApExel_TitleSet = False
                     End If
+                    Debug.WriteLine("Sheet Name: " & exportSheet.Name.Substring(1, 2))
+                    Debug.WriteLine("Sheet Count: " & exportExel.Application.Sheets.Count)
 
                     'Copia de la columna de titulos
                     If ApExel_TitleSet = False Then
-                        oSheet.Range("A4:A25").Copy()
+                        originalSheet.Range("A4:A25").Copy()
 
                         'Dim titulo = oSheet.Sheets(1).Range("A1").Copy()
-                        nSheet.Range("A1").PasteSpecial(Excel.XlPasteType.xlPasteValues)
-                        With nSheet.Columns("A")
+                        exportSheet.Range("A1").PasteSpecial(Excel.XlPasteType.xlPasteValues)
+                        With exportSheet.Columns("A")
                             .ColumnWidth = 36
                         End With
                         'nSheet.Columns("A").EntireColumn.AutoFit()
-                        nSheet.Range("A23").Value = "Fecha"
-                        nSheet.Range("A23").Font.Name = "Arial"
-                        nSheet.Range("A23").Font.Size = 8
+                        exportSheet.Range("A23").Value = "Fecha"
+                        exportSheet.Range("A23").Font.Name = "Arial"
+                        exportSheet.Range("A23").Font.Size = 8
 
                         ApExel_TitleSet = True
                     End If
 
                     'Copia de columna de information
-                    oSheet.Range("C4:C25").Copy()
-                    nSheet.Cells(1, columna).PasteSpecial(Excel.XlPasteType.xlPasteValues)
-                    With nSheet.Columns(columna)
+                    originalSheet.Range("C4:C25").Copy()
+                    exportSheet.Cells(1, columna).PasteSpecial(Excel.XlPasteType.xlPasteValues)
+                    With exportSheet.Columns(columna)
                         .ColumnWidth = 15
                     End With
 
                     'Fotmateo de la fecha
                     Dim fechaFinal As String = dia & "-" & mes & "-" & ano
-                    nSheet.Cells(23, columna).Value = DateSerial(Month:=mes, Day:=dia, Year:=ano)
-                    nSheet.Cells(23, columna).NumberFormat = "dd-mmm"
-                    nSheet.Cells(23, columna).font.Name = "Arial"
-                    nSheet.Cells(23, columna).font.Size = 8
+                    exportSheet.Cells(23, columna).Value = DateSerial(Month:=mes, Day:=dia, Year:=ano)
+                    exportSheet.Cells(23, columna).NumberFormat = "dd-mmm"
+                    exportSheet.Cells(23, columna).font.Name = "Arial"
+                    exportSheet.Cells(23, columna).font.Size = 8
 
                     columna += 1
 
-                    nSheet.Move(After:=nBook.Worksheets(nBook.Worksheets.Count))
+                    ' por defecto esta hoja de trabajo se colocará delante de la primera
+                    ' el siguiente código lo moverá atras la hoja actual
+                    exportSheet.Move(After:=exportBook.Worksheets(exportBook.Worksheets.Count))
 
-                    oBook.Close()
-                    oBook = Nothing
+                    ' Cierre del libro original
+                    originalBook.Close()
+                    originalBook = Nothing
 
+                    ' Variables ProgressBar
+                    ProgressBar1.Value = Cuenta
+                    Cuenta = Cuenta + 1
+                    Label2.Text = ProgressBar1.Value & (" %")
                 Next
 
+                Label1.Visible = True
+
+                ' Guardado del exel export y cerrar y vaciar variables
                 SaveFileDialog1.DefaultExt = "*.xlsx"
                 SaveFileDialog1.FileName = "PrecioElectricidad"
                 SaveFileDialog1.Filter = "Archivos de Exel (*.xlsx)|*.xlsx"
                 SaveFileDialog1.ShowDialog()
 
-                nBook.SaveAs(SaveFileDialog1.FileName)
-                MessageBox.Show("El archivo se creo y se guardo en:" & vbCrLf & SaveFileDialog1.FileName)
+                exportBook.SaveAs(SaveFileDialog1.FileName)
+                'MessageBox.Show("El archivo se creo y se guardo en:" & vbCrLf & SaveFileDialog1.FileName)
 
-                nBook.Close()
-                nBook = Nothing
-                ApExel = Nothing
-
-            End If
-        Catch ex As Exception
-            MessageBox.Show(ex.Message)
-        End Try
+                exportBook.Close()
+                exportBook = Nothing
+                exportExel = Nothing
+            Catch ex As Exception
+                MessageBox.Show(ex.Message)
+            End Try
+        End If
 
     End Sub
 
@@ -140,52 +159,22 @@ Public Class Form1
 
         oBook = oExcel.Workbooks.Add
 
-        ' first worksheet
-        If oExcel.Application.Sheets.Count() < 1 Then
-            oSheet = CType(oBook.Worksheets.Add(), Excel.Worksheet)
-        Else
-            oSheet = oExcel.Worksheets(1)
-        End If
-        oSheet.Name = "one"
-        oSheet.Range("B1").Value = "First One"
+        For number As Double = 1 To 4 Step +1
 
-        oSheet.Move(After:=oBook.Worksheets(oBook.Worksheets.Count))
+            If oExcel.Application.Sheets.Count() < number Then
+                oSheet = CType(oBook.Worksheets.Add(), Excel.Worksheet)
+            Else
+                oSheet = oExcel.Worksheets(number)
+            End If
+            oSheet.Name = number
+            oSheet.Range("B1").Value = number
 
-        ' second
-        If oExcel.Application.Sheets.Count() < 2 Then
-            oSheet = CType(oBook.Worksheets.Add(), Excel.Worksheet)
-        Else
-            oSheet = oExcel.Worksheets(2)
-        End If
-        oSheet.Name = "two"
-        oSheet.Range("B1").Value = "Second one"
+            ' por defecto esta hoja de trabajo se colocará delante de la primera
+            ' el siguiente código lo moverá atras la hoja actual
 
-        oSheet.Move(After:=oBook.Worksheets(oBook.Worksheets.Count))
+            oSheet.Move(After:=oBook.Worksheets(oBook.Worksheets.Count))
 
-        ' third
-        If oExcel.Application.Sheets.Count() < 3 Then
-            oSheet = CType(oBook.Worksheets.Add(), Excel.Worksheet)
-        Else
-            oSheet = oExcel.Worksheets(3)
-        End If
-        oSheet.Name = "three"
-        oSheet.Range("B1").Value = "Thrid"
-
-        oSheet.Move(After:=oBook.Worksheets(oBook.Worksheets.Count))
-
-        ' next
-        If oExcel.Application.Sheets.Count() < 4 Then
-            oSheet = CType(oBook.Worksheets.Add(), Excel.Worksheet)
-        Else
-            oSheet = oExcel.Worksheets(4)
-        End If
-        oSheet.Name = "four"
-        oSheet.Range("B1").Value = "Four"
-
-        ' by default this worksheet will be placed in front of the first
-        ' the code below will move it after third one
-
-        oSheet.Move(After:=oBook.Worksheets(oBook.Worksheets.Count))
+        Next
 
         oBook.SaveAs(fileTest)
         oBook.Close()
